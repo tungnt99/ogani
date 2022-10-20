d1e1f16e2d88b9b5329423abde4321deb0f02086
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-10-20 11:25:18 +0700

    push code register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,5 +9,20 @@ class LoginController extends Controller
 {
     public function login(){
         return view('layouts.app');
+    }
+    public function addAccount(Request $request){
+        $id = $request->id;
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+        $phone_number = $request->phone_number;
+        DB::table('users')->insert([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'phone_number' => $phone_number,
+        ]);
+        return redirect('/login');
+
     }
 }
