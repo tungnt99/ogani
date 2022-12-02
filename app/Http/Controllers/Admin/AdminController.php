61b523586bba24ff2c4e8d59ce2c4deb3e01f59e
Tùng <92722248+tungnt99@users.noreply.github.com>
2022-12-02 11:42:41 +0700

    push code logout

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,51 +2,52 @@
 
 namespace App\Http\Controllers\Admin;
 
+use DB;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+
 class AdminController extends Controller
 {
-    public function dashboard() {
-        if(!Auth::check())
-        {
+    public function dashboard(Request $request)
+    {
+        $role = DB::table('users')->select('role_name')->get();
+        if (!Auth::check() && !$role === 'admin') {
             return view('backend.login');
-        }
-        else
-        {
+        } else {
             return view('backend.dashboard');
         }
     }
     // login
-    public function login(){
+    public function login()
+    {
         // return ("ABC");
         return view('backend.login');
     }
-    public function loginAdmin(Request $request){
-        $this->validate($request,[
+    public function loginAdmin(Request $request)
+    {
+        $this->validate($request, [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
         $auth = array(
-            'email' =>$request->get('email'),
+            'email' => $request->get('email'),
             'password' => $request->get('password'),
         );
-        if(Auth::attempt($auth) && Auth::User()->role_name === 'admin'){
+        if (Auth::attempt($auth) && Auth::User()->role_name === 'admin') {
             return redirect()->route('backend.dashboard');
-
-        } else{
-            return back()->with('status','Invalid login details');
+        } else {
+            return back()->with('status', 'Invalid login details');
         }
     }
 
-    public function logout() {
-        if(Auth::logout())
-        {
-            return redirect()->route('backend.login');
-        }
-        return redirect()->route('admin.index');
+    public function logoutAdmin(Request $request)
+    {
+        $request->session()->flush();
+        return redirect()->route('backend.login');
     }
 
     // public function register(){
